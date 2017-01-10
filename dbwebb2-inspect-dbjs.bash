047549427c7378f37687c095a23976683912b1a6
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-01-10 12:17:46 +0100

    adding sources for inspect dbjs and oopython

---
+++ dbwebb2-inspect-dbjs.bash
@@ -0,0 +1,97 @@
+#---------------------------- INSPECT dbjs START --------------------------
+#
+# Test general
+#
+function dbjs()
+{
+    inspectIntro
+}
+
+
+
+#
+# Test general, last in sequence
+#
+function dbjslast()
+{
+    inspectMe "me/redovisa" "me.html" "redovisning.html" "" "redovisa"
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom01()
+{
+    inspectLab "uppgift/skapa-din-egen-sandbox-for-javascript-testprogram" "sandbox" "index.html" ""
+
+    inspectLab "uppgift/gor-svenska-flaggan-i-html-och-css-dbjs" "flag1" "index.html" ""
+
+    inspectLab "uppgift/javascript-med-variabler-loopar-och-inbyggda-funktioner-1" "javascript1" "answer.js" ""
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom02()
+{
+    # jetty kunskap/kom-igang-med-databasen-sqlite, boats.sqlite, boats.sql
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom03()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom04()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom05()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom06()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function dbjskmom10()
+{
+    :
+}
+
+
+
+#---------------------------- INSPECT DESIGN END ----------------------------
---
+++ dbwebb2-inspect-oopython.bash
@@ -0,0 +1,92 @@
+#---------------------------- INSPECT DBJS START --------------------------
+#
+# Test general
+#
+function oopython()
+{
+    inspectIntro
+}
+
+
+
+#
+# Test general, last in sequence
+#
+function oopythonlast()
+{
+    inspectMe "me/flask" "app.cgi" "" "" "flask"
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom01()
+{
+    inspectLab "uppgift/python-med-objekt-och-klasser" "lab1" "answer.py" "python3 answer.py"
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom02()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom03()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom04()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom05()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom06()
+{
+    :
+}
+
+
+
+#
+# Test kmom
+#
+function oopythonkmom10()
+{
+    :
+}
+
+
+
+#---------------------------- INSPECT DESIGN END ----------------------------
