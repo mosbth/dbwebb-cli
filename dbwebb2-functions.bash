1ad65f7c5e031efa5e94f49bd54b2d5052aa7194
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-03-14 09:47:58 +0100

    Corrected get_realpath.

--- dbwebb2-functions.bash
+++ dbwebb2-functions.bash
@@ -48,7 +48,7 @@ publishChmod()
 #
 function get_realpath()
 {
-    [[ ! -f "$1" ]] && return 1 # failure : file does not exist.
+    [[ ! -e "$1" ]] && return 1 # failure : file does not exist.
     [[ -n "$no_symlinks" ]] && local pwdp='pwd -P' || local pwdp='pwd' # do symlinks.
     echo "$( cd "$( echo "${1%/*}" )" 2>/dev/null; $pwdp )"/"${1##*/}" # echo result.
     return 0 # success
--- dbwebb2-version.bash
+++ dbwebb2-version.bash
@@ -1,4 +1,4 @@
 #
 # Current version
 #
-DBW_VERSION="v1.9.86 (2017-03-14)"
+DBW_VERSION="v1.9.87 (2017-03-14)"
