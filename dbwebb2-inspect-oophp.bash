4e2c7aaed694fc33e2909c4cd9531c40cb96a911
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-04-04 00:27:44 +0200

    Add repo tests on oophp.

--- dbwebb2-inspect-oophp.bash
+++ dbwebb2-inspect-oophp.bash
@@ -20,10 +20,50 @@ function oophplast()
 
 
 #
+#
+#
+function oophpRepo()
+{
+    local repo="$1"
+    local target="me/$repo"
+    
+    headerForTest "-- Repo $repo" "-- ${DBW_WWW}$DBW_COURSE/$target" 
+    openFilesInEditor "$target"
+    printUrl "" "$target"  
+
+    isGitRepo "$target"
+    hasGitTagBetween "$EXEC_DIR/$repo" "$2" "$3" > /dev/null
+    if [ ! -z "$4" ]; then
+        hasGitTagBetween "$EXEC_DIR/$repo" "$4" "$5" > /dev/null
+    fi
+
+    if [ ! -z "$4" ]; then
+        tag=$( hasGitTagBetween "$EXEC_DIR/$repo" "$2" "$5" )
+    else
+        tag=$( hasGitTagBetween "$EXEC_DIR/$repo" "$2" "$3" )
+    fi
+
+    inspectCommand "" "$EXEC_DIR/$repo" "git remote -v" ""
+    inspectCommand "" "$EXEC_DIR/$repo" "git tag" ""
+    inspectCommand "" "$EXEC_DIR/$repo" "git status" ""
+    inspectCommand "" "$EXEC_DIR/$repo" "git log -n 20 --pretty=format:\"%h_%ad_:_%s%d_[%an]\" --graph --date=short" ""
+    #inspectCommand "" "$EXEC_DIR/$repo" "git stash" ""
+    #inspectCommand "" "$EXEC_DIR/$repo" "git checkout -b inspect $tag" ""
+
+    # All repos does not include make test
+    # Make test need test environment
+    #inspectCommand "Makefile" "$EXEC_DIR/$repo" "make test" ""
+}
+
+
+
+#
 # Test kmom
 #
 function oophpkmom01()
 {
+    oophpRepo "anax-lite" "1.0.0" "2.0.0"
+
     inspectExerciseHeader "guess" "uppgift/gissa-numret" "$KMOM/guess"
     printUrl "index.php" "me/$KMOM/guess"
 
@@ -39,6 +79,8 @@ function oophpkmom01()
 #
 function oophpkmom02()
 {
+    oophpRepo "anax-lite" "2.0.0" "3.0.0"
+
     inspectExerciseHeader "skolan" "uppgift/kom-igang-med-sql" "kmom01/skolan"
     viewFileContent "skolan.sql" "me/kmom01/skolan"
     # should execute the sql-file skolan/skolan.sql
@@ -51,6 +93,8 @@ function oophpkmom02()
 #
 function oophpkmom03()
 {
+    oophpRepo "anax-lite" "3.0.0" "4.0.0"
+
     inspectExerciseHeader "skolan" "uppgift/kom-igang-med-sql" "kmom01/skolan"
     viewFileContent "skolan.sql" "me/kmom01/skolan"
     # should execute the sql-file skolan/skolan.sql
--- dbwebb2-version.bash
+++ dbwebb2-version.bash
@@ -1,4 +1,4 @@
 #
 # Current version
 #
-DBW_VERSION="v1.9.96 (2017-04-03)"
+DBW_VERSION="v1.9.97 (2017-04-04)"
